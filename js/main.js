f2069f02a9e06c0d7f9f489f55711a39952db108
LAPTOP-2G9M5ROS\surya <suryateja.0153@gmail.com>
2021-03-21 21:03:05 -0400

    Assignment - 3

--- js/main.js
+++ js/main.js
@@ -1,14 +1,28 @@
-/**
- * Your JS code here
- */
+// // When the user scrolls the page, execute myFunction
+// window.onscroll = function() {myFunction()};
+//
+// // Get the header
+// var header = document.getElementById("header");
+//
+// // Get the offset position of the navbar
+// var sticky = header.offsetTop;
+//
+// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
 
 /**
  * Sample code
  */
-document.addEventListener('DOMContentLoaded', function() {
-  var ulElement = document.getElementById('links');
-  var liElement = document.createElement('li');
-  liElement.appendChild(document.createTextNode('created by main.js'));
-
-  ulElement.appendChild(liElement);
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//   var ulElement = document.getElementById('links');
+//   var liElement = document.createElement('li');
+//   liElement.appendChild(document.createTextNode('created by main.js'));
+//
+//   ulElement.appendChild(liElement);
+// });
